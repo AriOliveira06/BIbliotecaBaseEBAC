75616c0b3318e7ea5cc8fbf9524d2574374427cd
AriOliveira06 <ariclenesoliveirabezerra@gmail.com>
2026-06-26 03:04:52 -0300

    Adicionando estruturação das informações do BackEnd

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
 meus_livros = dict()
+
+class LivrosFormato(BaseModel):
+    nome_livro: str
+    autor_livro: str
+    ano_livro: int
 
 @app.get('/livros')
 def get_livros():
@@ -12,28 +19,23 @@ def get_livros():
         return {'livros': meus_livros}
     
 @app.post('/adicionar')
-def post_livros(id_livro: int, nome_livro: str, autor_livro: str, ano_livro: int):
+def post_livros(id_livro: int, livro : LivrosFormato):
     if id_livro in meus_livros:
         raise HTTPException(status_code = 400, detail = 'Esse livro ja foi registrado.')
     else:
-        meus_livros[id_livro] = {'nome_livro': nome_livro, 'autor_livro': autor_livro, 'ano_livro': ano_livro}
+        meus_livros[id_livro] = livro.dict()
         return {'message' : 'Livro adicionado com sucesso'}
 @app.put('/atualizar/{id_livro}')
-def put_livros(id_livro: int, nome_livro: str, autor_livro: str, ano_livro: int):
+def put_livros(id_livro: int, livro: LivrosFormato):
     meu_livro = meus_livros.get(id_livro)
     if id_livro not in meus_livros:
         raise HTTPException(status_code = 404, detail = 'O livro não foi encontrado.')
     else:
-        if nome_livro:
-            meus_livros[id_livro]['nome_livro'] = nome_livro
-        if autor_livro:
-            meus_livros[id_livro]['autor_livro'] = autor_livro
-        if ano_livro:
-            meus_livros[id_livro]['ano_livro'] = ano_livro
+        meu_livro[id_livro] = livro.dict()
             
     return {'message': 'Livro atualizado com sucesso.'}
 
-@app.delete('/deletar/{id_livro}')
+@app.delete('/deletar/{id_livro}')          
 def delete_livros(id_livro: int):
     if id_livro not in meus_livros:
         raise HTTPException(status_code = 404, detail = 'O livro não foi encontrado.')
